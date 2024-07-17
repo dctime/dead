dc3dc0ee1f4e1e7c19c559af153edc059bdb8e5b
DCtime <daniel.chu916@gmail.com>
2024-07-17 14:22:28 +0800

    Fix: Faster Delete Bullets

--- lib/src/DEAD_bullet_director.cpp
+++ lib/src/DEAD_bullet_director.cpp
@@ -1,50 +1,43 @@
 #include "DEAD_bullet.h"
 #include <DEAD_bullet_director.h>
-#include <SDL2/SDL_log.h>
 #include <DEAD_game.h>
-#include <iostream>
+#include <SDL2/SDL_log.h>
 #include <set>
 
-DEAD_BulletDirector::DEAD_BulletDirector(DEAD_Game* game) 
-  : game(game) {
-}
+DEAD_BulletDirector::DEAD_BulletDirector(DEAD_Game *game) : game(game) {}
 
-void DEAD_BulletDirector::registerBullet(DEAD_Bullet* bullet) {
+void DEAD_BulletDirector::registerBullet(DEAD_Bullet *bullet) {
   this->bullets.insert(bullet);
   SDL_Log("[Bullet Director] register a bullet");
 }
 
-int DEAD_BulletDirector::bulletCount() {
-  return this->bullets.size();
-}
+int DEAD_BulletDirector::bulletCount() { return this->bullets.size(); }
 
 DEAD_BulletDirector::~DEAD_BulletDirector() {
-  for (DEAD_Bullet* bullet : this->bullets) {
+  for (DEAD_Bullet *bullet : this->bullets) {
     delete bullet;
   }
   this->bullets.clear();
 }
 
-std::set<DEAD_Bullet*> DEAD_BulletDirector::getBullets() {
+std::set<DEAD_Bullet *> DEAD_BulletDirector::getBullets() {
   return this->bullets;
 }
 
 void DEAD_BulletDirector::tickBullets() {
-  bool hasCollision = false;
-  for (std::set<DEAD_Bullet*>::iterator bullet = this->bullets.begin();
-       bullet != this->bullets.end(); bullet++) {
-    if (this->game->getCollisionDirector()->bulletCheckCollision(*bullet).size() != 0) {
-      delete *bullet;
-      this->bullets.erase(*bullet);
-      hasCollision = true;
+  std::set<DEAD_Bullet *> tempRemoveBullet;
+  for (DEAD_Bullet *bullet : this->bullets) {
+    if (this->game->getCollisionDirector()
+            ->bulletCheckCollision(bullet)
+            .size() != 0) {
+      tempRemoveBullet.insert(bullet);
+    } else {
+      bullet->tickBullet();
     }
-
-    if (hasCollision) break;
-    (*bullet)->tickBullet();
   }
-  if (hasCollision) {
-    std::cout << "Run TickBullets again" << std::endl;
-    tickBullets();
+
+  for (DEAD_Bullet* removingBullet : tempRemoveBullet) {
+    this->bullets.erase(removingBullet);
+    delete removingBullet;
   }
 }
-
