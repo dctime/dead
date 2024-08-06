05dac48a4f7f3a1ba83b8d77e57de435fafd0afa
DCtime <daniel.chu916@gmail.com>
2024-08-06 21:03:21 +0800

    Feature: Game Launcher and SQL

--- main.cpp
+++ main.cpp
@@ -7,12 +7,119 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_log.h>
 #include <SDL2/SDL_video.h>
+#include <cppconn/connection.h>
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <gtk/gtk.h>
+#include <iostream>
 #include <memory>
+#include <mysql_connection.h>
+#include <mysql_driver.h>
+#include <string>
 
-int main() {
-  std::shared_ptr<DEAD_GameBuilder> gameBuilder =
-      std::make_shared<DEAD_GameBuilder>();
-  std::shared_ptr<DEAD_Game> game = gameBuilder->build();
+int main(int argc, char **argv) {
+  sql::mysql::MySQL_Driver *driver;
+  sql::Connection *conn;
+  sql::ConnectOptionsMap connectionProperties;
 
-  game->run();
+  void onMainWindowDestory(GtkWidget * window);
+  void onPlayButtonClicked(GtkButton * window);
+
+  driver = sql::mysql::get_mysql_driver_instance();
+  conn = driver->connect("sql12.freemysqlhosting.net", "sql12724310",
+                         "xffMZcQICr");
+  conn->setSchema("sql12724310");
+
+  GError *err = NULL;
+  GtkWidget *window;
+  GtkListStore *liststoreLongest;
+  GtkTreeViewColumn *columnLongestName;
+  GtkTreeViewColumn *columnLongestTime;
+  GtkCellRenderer *cellRendererLongestName;
+  GtkCellRenderer *cellRendererLongestTime;
+  GtkButton *playButton;
+  GtkBuilder *builder;
+  gtk_init(&argc, &argv);
+
+  builder = gtk_builder_new();
+  gtk_builder_add_from_file(builder, "assets/glade/Test.glade", &err);
+  if (err != NULL) {
+    std::cout << "Error: " << err->message << std::endl;
+  }
+
+  window = GTK_WIDGET(gtk_builder_get_object(builder, "mainWindow"));
+  playButton = GTK_BUTTON(gtk_builder_get_object(builder, "playButton"));
+
+  g_signal_connect(window, "destroy", G_CALLBACK(onMainWindowDestory), NULL);
+  g_signal_connect(playButton, "clicked", G_CALLBACK(onPlayButtonClicked),
+                   NULL);
+
+  gtk_builder_connect_signals(builder, NULL);
+
+  liststoreLongest =
+      GTK_LIST_STORE(gtk_builder_get_object(builder, "liststoreLongest"));
+  columnLongestName = GTK_TREE_VIEW_COLUMN(
+      gtk_builder_get_object(builder, "columnLongestName"));
+  columnLongestTime = GTK_TREE_VIEW_COLUMN(
+      gtk_builder_get_object(builder, "columnLongestTime"));
+  cellRendererLongestName = GTK_CELL_RENDERER(
+      gtk_builder_get_object(builder, "cellRendererLongestName"));
+  cellRendererLongestTime = GTK_CELL_RENDERER(
+      gtk_builder_get_object(builder, "cellRendererLongestTime"));
+  gtk_tree_view_column_add_attribute(columnLongestName, cellRendererLongestName,
+                                     "text", 0);
+  gtk_tree_view_column_add_attribute(columnLongestTime, cellRendererLongestTime,
+                                     "text", 1);
+
+  std::cout << "TIME FOR SQL" << std::endl;
+
+  std::unique_ptr<sql::Statement> statement(conn->createStatement());
+  std::unique_ptr<sql::ResultSet> resultSet(statement->executeQuery(
+      "SELECT * FROM LongestSurvivalTime ORDER BY TimeInSeconds DESC LIMIT 10"));
+  std::string name;
+  int timeInSeconds;
+
+  try {
+    GtkTreeIter iterLongest;
+    while (resultSet->next()) {
+      name = resultSet->getString("Name");
+      timeInSeconds = resultSet->getInt("TimeInSeconds");
+      const char *nameChars = name.c_str();
+      std::string timeFormatted = std::to_string(timeInSeconds);
+      const char *timeFormattedChars = timeFormatted.c_str();
+      std::cout << "Name: " << nameChars << ", TimeInSeconds: " << timeInSeconds
+                << std::endl;
+      gtk_list_store_append(liststoreLongest, &iterLongest);
+      gtk_list_store_set(liststoreLongest, &iterLongest, 0, nameChars, -1);
+      gtk_list_store_set(liststoreLongest, &iterLongest, 1, timeFormattedChars,
+                         -1);
+      std::cout << "added a row" << std::endl;
+    }
+  } catch (sql::SQLException &e) {
+    std::cout << "# Err: " << e.what();
+    std::cout << " (My SQL Error Code: " << e.getErrorCode();
+    std::cout << ", SQL_STATE: " << e.getSQLState() << " )" << std::endl;
+  }
+
+  gtk_widget_show_all(window);
+  gtk_main();
+
+  // std::shared_ptr<DEAD_GameBuilder> gameBuilder =
+  //     std::make_shared<DEAD_GameBuilder>();
+  // std::shared_ptr<DEAD_Game> game = gameBuilder->build();
+  //
+  // game->run();
+  delete conn;
+}
+
+void onMainWindowDestory(GtkWidget *window) {
+  std::cout << "Hello!" << std::endl;
+  gtk_main_quit();
+}
+
+void onPlayButtonClicked(GtkButton *window) {
+  std::cout << "Button Activated" << std::endl;
+  gtk_main_quit();
 }
